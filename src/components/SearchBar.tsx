792c7b1a2a8bd0b30176747141cdcffda6555f43
intellect-sam <alukoolakunlesamuel@gmail.com>
2024-05-24 13:40:13 +0100

    Made some changes

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,34 +1,60 @@
 import {
+  Button,
   Input,
   InputGroup,
   InputLeftAddon,
   InputLeftElement,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
 } from '@chakra-ui/react';
 
 import React from 'react';
 import { IoSearchOutline } from 'react-icons/io5';
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
 
 const SearchBar = () => {
   return (
     <>
       <div>
-        <form action="">
-          <div className="relative mt-2 rounded-md shadow-sm ">
-            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-              <span className="text-gray-500 sm:text-sm">
-                <IoSearchOutline />
-              </span>
+        <form
+          action=""
+          className="flex flex-col gap-5 justify-center tablet, laptop, tablet:flex-row">
+          <Menu>
+            <MenuButton
+              as={Button}
+              rightIcon={<MdOutlineKeyboardArrowDown />}
+              textColor={'gray'}
+              backgroundColor={'#F5F5F5'}
+              textStyle={'12px'}>
+              Category
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Clothings and Accessories</MenuItem>
+              <MenuItem>Gadgets </MenuItem>
+              <MenuItem>Electronics</MenuItem>
+              <MenuItem>Food </MenuItem>
+              <MenuItem></MenuItem>
+            </MenuList>
+          </Menu>
+          <div className="flex justify-between gap-4">
+            <div className="relative rounded-md ">
+              <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                <span className="text-gray-500">
+                  <IoSearchOutline />
+                </span>
+              </div>
+              <input
+                type="text"
+                className="block rounded-md  w-[200px] py-[10px] pl-10 pr-20 laptop, tablet:w-[458px] text-[13px]   bg-gry  placeholder:text-gray-400"
+                placeholder="Search"
+              />
             </div>
-            <input
-              type="text"
-              className="block w-full rounded-md  py-1 pl-10 pr-20 md:w-[458px]   bg-gry  placeholder:text-gray-400   sm:text-sm sm:leading-6"
-              placeholder="Search"
-            />
-            <div className="absolute inset-y-0 right-0 flex items-center text-primary text-sm">
-              <button className="bg-second-col h-full rounded-md px-4">
-                Search
-              </button>
-            </div>
+
+            <button className="bg-second-col  rounded-md px-10 text-[13px] text-primary">
+              Search
+            </button>
           </div>
         </form>
       </div>
