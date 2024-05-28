9164c55d87a8de27a6420058c702b5c1343af3e0
intellect-sam <alukoolakunlesamuel@gmail.com>
2024-05-28 12:31:41 +0100

    home refactor

--- src/components/HeroPage.tsx
+++ src/components/HeroPage.tsx
@@ -11,16 +11,16 @@ const HeroPage = () => {
         <Image
           src={circle}
           alt=""
-          className="w-80 laptop, tablet:w-[750px]"
+          className="w-80 laptop, tablet:w-[900px]"
         />
       </div>
 
-      <div className="w-full p-4 laptop, tablet:py-[200px] flex flex-col  laptop:gap-0 laptop, tablet:flex-row text-gray-dark  laptop:justify-center my-16">
-        <div className="laptop, tablet:min-w-[500px]">
+      <div className="w-full p-4 laptop, tablet:py-[160px] flex flex-col  laptop:gap-0 laptop, tablet:flex-row text-gray-dark  laptop:justify-center my-16">
+        <div className="laptop, tablet:min-w-[500px] relative left-[120px]">
           <h2 className="custom-h1 laptop:text-4xl laptop:leading-tight laptop:text-left">
             Show your Trust Score. Write Reviews. Find Trustworthy Business.
           </h2>
-          <p className="custom-p sm:py-5 laptop:px-0 laptop:max-w-[400px] laptop:text-left leading-relaxed laptop:leading-[35px]  laptop:text-lg laptop:font-[2px]">
+          <p className="custom-p sm:py-5 laptop:px-0 laptop:max-w-[400px] laptop:text-left leading-relaxed laptop:leading-[35px]  laptop:text-[20px] laptop:font-[1px]">
             Never get scammed again. Ask for their trust worthy vendors. Check
             their trust score before sliding into their DM
           </p>
